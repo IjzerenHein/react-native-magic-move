e0a450fb8833c3ac7e391425c22bf983c887da58
Hein Rutjes <hrutjes@gmail.com>
2019-01-02 16:54:53 +0100

    Removed blackHole transition

--- examples/src/magic-move/transitions/index.js
+++ examples/src/magic-move/transitions/index.js
@@ -3,11 +3,9 @@ import flipTransition from "./flip";
 import dissolveTransition from "./dissolve";
 import shrinkAndGrowTransition from "./shrinkAndGrow";
 import squashAndStretchTransition from "./squashAndStretch";
-import blackHoleTransition from "./blackHole";
 
 export const morph = morphTransition;
 export const flip = flipTransition;
 export const dissolve = dissolveTransition;
 export const shrinkAndGrow = shrinkAndGrowTransition;
 export const squashAndStretch = squashAndStretchTransition;
-export const blackHole = blackHoleTransition;
