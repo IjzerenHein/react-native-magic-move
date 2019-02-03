5f3cc3a924ed14d11c474592dc28b4e0e930d92d
Hein Rutjes <hrutjes@gmail.com>
2019-02-03 17:39:29 +0100

    Update

--- src/transitions/index.js
+++ src/transitions/index.js
@@ -1,22 +1,22 @@
 import moveTransition from "./move";
-import scaleTransition from "./scale";
 import morphTransition from "./morph";
 import flipTransition from "./flip";
 import dissolveTransition from "./dissolve";
 import shrinkAndGrowTransition from "./shrinkAndGrow";
 import squashAndStretchTransition from "./squashAndStretch";
+import scaleTransition from "./scale";
 // import foldTransition from "./fold";
 // import fadeTransition from "./fade";
 // import blurTransition from "./blur";
 // import dottedTransition from "./dotted";
 
-export const move = moveTransition;
-export const scale = scaleTransition;
+export const move = moveTransition; // default
 export const morph = morphTransition;
 export const flip = flipTransition;
 export const dissolve = dissolveTransition;
 export const shrinkAndGrow = shrinkAndGrowTransition;
 export const squashAndStretch = squashAndStretchTransition;
+export const scale = scaleTransition; // DEPRECATED
 // export const fold = foldTransition;
 // export const fade = fadeTransition;
 // export const blur = blurTransition;
